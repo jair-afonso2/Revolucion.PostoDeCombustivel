8d0c9a9d42ed1ef880769aadde7a15f77625233d
Caio Chaves Sampaio <37795527876@jpmorgan.sp>
2017-11-16 20:53:56 -0200

    Classe Endereço()

--- Dominio/Endereco.cs
+++ Dominio/Endereco.cs
@@ -2,6 +2,24 @@ namespace Dominio
 {
     public class Endereco
     {
-        
+        public string logradouro { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; }
+        public string cep { get; set; }
+
+        public Endereco(){}
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cidade, string estado, string cep)
+        {
+            this.logradouro = logradouro;
+            this.numero = numero;
+            this.complemento = complemento;
+            this.bairro = bairro;
+            this.cidade = cidade;
+            this.estado = estado;
+            this.cep = cep;
+        }
     }
 }
